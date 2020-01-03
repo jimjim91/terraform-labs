4ad30438896463f8cdc2ed54ecb31b816a3cc74b
James Moran <James.Moran@derivco.co.uk>
2020-01-03 16:01:29 +0000

    added count.index

--- webapps.tf
+++ webapps.tf
@@ -13,25 +13,26 @@ resource "random_string" "webapprnd" {
 }
 
 resource "azurerm_app_service_plan" "free" {
-    name                = "plan-free-${var.loc}"
-    location            = "${var.loc}"
-    resource_group_name = "${azurerm_resource_group.webapps.name}"
-    tags                = "${azurerm_resource_group.webapps.tags}"
+  count               = length(var.webapplocs)
+  name                = "plan-free-${var.webapplocs[count.index]}"
+  location            = var.webapplocs[count.index]
+  resource_group_name = "${azurerm_resource_group.webapps.name}"
+  tags                = "${azurerm_resource_group.webapps.tags}"
 
-    kind                = "Linux"
-    reserved            = true
-    sku {
-        tier = "Free"
-        size = "F1"
-    }
+  kind     = "Linux"
+  reserved = true
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
 }
 
 resource "azurerm_app_service" "citadel" {
-    name                = "webapp-${random_string.webapprnd.result}-${var.loc}"
-    location            = "${var.loc}"
-    resource_group_name = "${azurerm_resource_group.webapps.name}"
-    tags                = "${azurerm_resource_group.webapps.tags}"
+  count               = length(var.webapplocs)
+  name                = "webapp-${random_string.webapprnd.result}-${var.webapplocs[count.index]}"
+  location            = var.webapplocs[count.index]
+  resource_group_name = "${azurerm_resource_group.webapps.name}"
+  tags                = "${azurerm_resource_group.webapps.tags}"
 
-    app_service_plan_id = "${azurerm_app_service_plan.free.id}"
+  app_service_plan_id = "${element(azurerm_app_service_plan.free.*.id, count.index)}"
 }
-
